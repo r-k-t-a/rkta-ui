bd118bc0411a37a689fc354d26db581a81b5c0e2
Sergei Droganov <serge@droganov.ru>
2018-11-22 22:01:46 +0300

    forwarding inline styles to node

--- src/lib/Atoms/Atom/index.js
+++ src/lib/Atoms/Atom/index.js
@@ -3,12 +3,14 @@ import { styled } from 'styletron-react';
 
 const Atom = ({
   element,
+  inlineStyles,
   style,
   theme,
   toggleTheme,
   uiRef,
   ...rest
-}) => createElement(element, { ...rest, ref: uiRef });
+}) => createElement(element, { ...rest, ref: uiRef, style: inlineStyles });
+
 Atom.defaultProps = { element: 'div' };
 
 export default styled(Atom, ({ style }) => style);
