6625513f7aa832cfc62bfe2127ed531b09ec5fdc
Sergei Droganov <serge@droganov.ru>
2019-06-25 16:29:25 +0300

    take paper shadows from mui

--- src/atoms/Paper/index.jsx
+++ src/atoms/Paper/index.jsx
@@ -3,17 +3,13 @@ import PropTypes from 'prop-types';
 
 import Font from '../Font';
 import withStyle from '../../Theme/withStyle';
+import shadows from './shadows';
 
 const modifyProps = ({ css, rize, size, theme, ...props }) => {
   const nextCss = {};
-  if (rize) {
-    const blur = Math.max(rize ** 3, 5);
+  if (rize && rize < shadows.length - 1) {
     Object.assign(nextCss, {
-      boxShadow: `
-        0px ${rize}px ${blur}px 0px rgba(0, 0, 0, 0.2),
-        0px ${rize * 2}px ${blur / 2}px 0px rgba(0, 0, 0, 0.14),
-        0px ${rize * 3}px ${blur / rize}1px ${-rize / 2}px rgba(0, 0, 0, 0.12)
-      `,
+      boxShadow: shadows[rize],
     });
   }
   if (size) {
