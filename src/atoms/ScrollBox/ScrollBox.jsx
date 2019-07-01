8fa11260276054e6072b6c53292a44c5f17de5a9
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-07-01 15:52:16 +0300

    added interactive to scrollbox

--- src/atoms/ScrollBox/ScrollBox.jsx
+++ src/atoms/ScrollBox/ScrollBox.jsx
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Atom from '../Atom';
 import media from '../../util/media';
 
-const getBoxCss = (css, x, y) =>
-  media({
+const getBoxCss = (css, x, y, interactive, needScroll) => {
+  const scroll = interactive ? needScroll : true;
+  return media({
     ...css,
     overflow: 'hidden',
-    overflowY: y ? 'scroll' : undefined,
-    overflowX: x ? 'scroll' : undefined,
+    overflowY: y && scroll ? 'scroll' : undefined,
+    overflowX: x && scroll ? 'scroll' : undefined,
     overflowScrolling: 'touch',
     position: 'relative',
     scrollBehavior: 'smooth',
     scrollSnapType: 'both mandatory',
     WebkitOverflowScrolling: 'touch',
   });
+};
 
-const ScrollBox = ({ css, x, y, ...rest }) => <Atom {...rest} css={getBoxCss(css, x, y)} />;
+const ScrollBox = ({ css, x, y, interactive, ...rest }) => {
+  const [needScroll, setNeedSCroll] = useState(false);
+  function handleStart() {
+    if (needScroll) return;
+    setNeedSCroll(true);
+  }
+  function handleEnd() {
+    if (!needScroll) return;
+    setNeedSCroll(false);
+  }
+  return (
+    <Atom
+      {...rest}
+      css={getBoxCss(css, x, y, interactive, needScroll)}
+      onMouseOver={handleStart}
+      onFocus={handleStart}
+      onTouchStart={handleStart}
+      onScroll={handleStart}
+      onMouseLeave={handleEnd}
+      onBlur={handleEnd}
+      onTouchEnd={handleEnd}
+    />
+  );
+};
 
 ScrollBox.displayName = 'ScrollBox';
 ScrollBox.propTypes = {
   children: PropTypes.node.isRequired,
   css: PropTypes.shape(),
+  interactive: PropTypes.bool,
   x: PropTypes.bool,
   y: PropTypes.bool,
 };
 ScrollBox.defaultProps = {
   css: null,
+  interactive: false,
   x: false,
   y: false,
 };
