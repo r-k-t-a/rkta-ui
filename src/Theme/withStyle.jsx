7fcdde027816068613ae53ca421c4c83590aa04d
Sergei Droganov <serge@droganov.ru>
2019-07-12 00:30:15 +0300

    reduce appears to work fater

--- src/Theme/withStyle.jsx
+++ src/Theme/withStyle.jsx
@@ -1,40 +1,39 @@
 import React, { forwardRef } from 'react';
-import memoize from 'lodash/memoize';
 
 import Context from './Context';
 import defaultRenderrer from './defaultRenderrer';
 import invariant from '../util/invariant';
 
-const modifyStyles = memoize((context, { children, ...props }, element, ref) => {
+const modifyStyles = (context, { children, ...props }, element, ref) => {
   const { defaultStyle, ...styles } = context.theme[element];
 
-  const nextProps = {};
-  const nextStyle = { ...defaultStyle };
+  // const nextProps = {};
+  // const nextStyle = { ...defaultStyle };
 
-  const keys = Object.keys(props);
+  // const keys = Object.keys(props);
 
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
-    const value = props[key];
-    if (value === true && key in styles) Object.assign(nextStyle, styles[key]);
-    else if (!(key in styles)) nextProps[key] = value;
-  }
+  // for (let i = 0; i < keys.length; i += 1) {
+  //   const key = keys[i];
+  //   const value = props[key];
+  //   if (value === true && key in styles) Object.assign(nextStyle, styles[key]);
+  //   else if (!(key in styles)) nextProps[key] = value;
+  // }
 
-  // const { nextStyle, nextProps } = Object.keys(props).reduce(
-  //   (acc, key) => {
-  //     const value = props[key];
-  //     if (value === true && key in styles) Object.assign(acc.nextStyle, styles[key]);
-  //     else if (!(key in styles)) Object.assign(acc.nextProps, { [key]: value });
-  //     return acc;
-  //   },
-  //   {
-  //     nextProps: {},
-  //     nextStyle: { ...defaultStyle },
-  //   },
-  // );
+  const { nextStyle, nextProps } = Object.keys(props).reduce(
+    (acc, key) => {
+      const value = props[key];
+      if (value === true && key in styles) Object.assign(acc.nextStyle, styles[key]);
+      else if (!(key in styles)) Object.assign(acc.nextProps, { [key]: value });
+      return acc;
+    },
+    {
+      nextProps: {},
+      nextStyle: { ...defaultStyle },
+    },
+  );
   const css = { ...nextStyle, ...props.css };
   return { ...nextProps, ...context, children, css, ref };
-});
+};
 
 const pickProps = (context, props, elementName, ref) => {
   if (elementName in context.theme) return modifyStyles(context, props, elementName, ref);
